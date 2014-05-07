8ab5038ae5a38c20a7f4c8461d6b7eda41f3f8ad
Giannis Melidis <giannismelidis@olery.com>
2014-05-07 15:29:36 +0200

    remove pre_build stuff.

--- core/site-packages/pre_build/VUKafParserPy/__init__.py
+++
@@ -1,7 +0,0 @@
-## version = 0.2
-## Added timestamp to function addLinguisitcProcessor
-## 24-april-2013 --> getSingleEntieies and getSingleProperties reads both entities/props in format
-## entities -> entity -> span -> target and entities -> entity -> references -> span
-####
-
-from KafParserMod import KafParser
